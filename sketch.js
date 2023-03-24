38e6654d134f5811f9fe1d8ce71c3f3de5b9d616
caizoryan <pashineaaryan@gmail.com>
2023-03-23 22:57:16 -0400

    ~init?

--- sketch.js
+++ sketch.js
@@ -1,92 +1,57 @@
-let margin = 50;
-let sqrSize = 50;
-let sqr, sqr2;
-let chars = "LAURA COOMBS".split("");
-let rotationTl = new Timeline();
-let rot = { angle: 0 };
-let sw = "cMoAoRm b2s8. o7c:a3d1up.mg d".split("");
-
+let t = 200;
+let trs = [];
 function setup() {
   createCanvas(1000, 1000);
-  setInterval(() => {
-    let temp = chars.slice();
-    chars = sw;
-    sw = temp;
-  }, 3000);
-  rotationTl.add(
-    new PropKeyframes(rot, "angle", [
-      [0, 700],
-      [180, 2700],
-      [180, 700],
-      [0, 2700],
-    ])
+  trs.push(
+    new TextTrail("LAURA", color(255, 200, 0), laura, 0),
+    new TextTrail("COOMBS", color(255, 200, 0), coombs, 0),
+    new TextTrail("MARCH", color(255), date, (coombs.length - 2) * t * 2)
   );
-  sqr = new Square(50, 0, 0, 500, 500, 50, 1);
-  sqr2 = new Square(50, 500, 500, width, height, 50, 1400);
-  rotationTl.loop();
-  rotationTl.animate();
-  rotationTl.setEasing("InOutCubic");
-  angleMode(DEGREES);
 }
 
 function draw() {
-  rotationTl.update();
-  background(0, 200);
-  fill(255);
-  textSize(40);
-  for (let i = 0; i < chars.length; i++) {
-    push();
-    translate(width / 2, height / 2);
-    rotate(rot.angle * i);
-    translate(-width / 2, 0);
-    text(chars[i], margin + i * 20, height / 4);
-    pop();
-  }
-  sqr.update();
-  sqr2.update();
+  background(250, 0, 50, 200);
+  for (const x of trs) x.update();
 }
 
-class Square {
-  constructor(size, contX, contY, contW, contH, margin, delay) {
-    this.size = size;
-    this.margin = margin;
-    this.contW = contW;
-    this.contH = contH;
-    this.contX = contX;
-    this.contY = contY;
-    this.delay = delay;
-    this.pos = { x: contX + margin, y: contY + margin };
+class TextTrail {
+  constructor(text, border, keyframes, delay) {
+    this.text = text;
+    this.border = border;
+    this.keyframes = keyframes;
+    this.trail = [];
+    this.pos = { x: keyframes[0].x, y: keyframes[0].y };
     this.tl = new Timeline();
-    this.init();
-  }
-  init() {
-    this.tl.add(
-      new PropKeyframes(this.pos, "x", [
-        [this.contW - this.margin - this.size, 700],
-        [this.contW - this.margin - this.size, 700],
-        [this.contX + this.margin, 700],
-        [this.contX + this.margin, 700],
-      ])
-    );
-    this.tl.add(
-      new PropKeyframes(this.pos, "y", [
-        [this.contY + this.margin, 700],
-        [this.contH - this.margin - sqrSize, 700],
-        [this.contH - this.margin - sqrSize, 700],
-        [this.contY + this.margin, 700],
-      ])
-    );
-    this.tl.setEasing("InOutCubic");
-    this.tl.loop();
+    this.x = [];
+    this.y = [];
+    for (const key of this.keyframes) {
+      this.x.push([key.x, t]);
+      this.x.push([key.x, t]);
+      this.y.push([key.y, t]);
+      this.y.push([key.y, t]);
+    }
+    this.tl.add(new PropKeyframes(this.pos, "x", this.x));
+    this.tl.add(new PropKeyframes(this.pos, "y", this.y));
+    // this.tl.loop();
     setTimeout(() => {
       this.tl.animate();
-    }, this.delay);
+    }, delay);
+    this.tl.setEasing("InOutQuad");
   }
   update() {
     this.tl.update();
-    rect(this.pos.x, this.pos.y, this.size, this.size);
+    if (this.trail.length > 50) this.trail.splice(0, 1);
+    this.trail.push({ x: this.pos.x, y: this.pos.y });
+    for (const p of this.trail) {
+      fill(this.border);
+      stroke(this.border);
+      strokeWeight(20);
+      strokeJoin(ROUND);
+      textSize(100);
+      text(this.text, p.x, p.y);
+      fill(0);
+      noStroke();
+    }
+    text(this.text, this.pos.x, this.pos.y);
   }
 }
-
-class Block {}
-class Letters {}
